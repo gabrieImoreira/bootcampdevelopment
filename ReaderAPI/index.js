8a1a15d464d01f9eb826fd6203710163111cadfe
gams99 <gabriel.antonioms@gmail.com>
2020-12-09 00:18:17 -0300

    create json states is running

--- ReaderAPI/index.js
+++ ReaderAPI/index.js
@@ -1,5 +1,22 @@
 import {promises as fs} from "fs";
 
+let allStates = [];
+
+filterStates();
+async function filterStates(){
+  const jsonStates = JSON.parse(await fs.readFile("Estados.json"));
+  
+  const obj = 2;
+  jsonStates.forEach(state =>{
+    const {ID, Sigla, Nome} = state;
+    fs.writeFile("./States/"+Sigla+".json", JSON.stringify(obj));
+  });
+}
+//
 async function StatesAndCities(){
-  try
+  try{
+
+  }catch{
+
+  }
 }
